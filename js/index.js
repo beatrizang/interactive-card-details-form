47a29c72e844076b043b3de964a9de3aa504f7f3
BeatrizG <beatrizgarciaang0@gmail.com>
2022-12-28 20:59:59 -0300

    Se agrega responsive 375px

--- js/index.js
+++ js/index.js
@@ -14,6 +14,61 @@ const correctLength = (number,length) => {
     return (number.length == length)?true : false;
 }
 
+const correctYear = (year) => {
+    var y = new Date().getFullYear();
+    return ('20' + year >= y)?true : false;
+}
+
+const onlyLetters = (string) => {
+    let regex = /^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$/g;
+    return regex.test(string);
+}
+
+
+const name = document.getElementById('input-name');
+const cardNameEnter = document.getElementById('card-name');
+
+const number = document.getElementById('input-number');
+const cardNumberEnter = document.getElementById('card-number');
+
+
+const month = document.getElementById('input-month');
+const cardMonthEnter = document.getElementById('card-month');
+
+const year = document.getElementById('input-year');
+const cardYearEnter = document.getElementById('card-year');
+
+
+const cvc = document.getElementById('input-cvc');
+const cardCvcEnter = document.getElementById('card-cvc');
+
+
+name.addEventListener('input', updateName);
+number.addEventListener('input', updateNumber);
+cvc.addEventListener('input', updateCvc);
+month.addEventListener('input', updateMonth);
+year.addEventListener('input', updateYear);
+
+function updateName(e) {
+    cardNameEnter.textContent = e.srcElement.value.toUpperCase();
+}
+
+function updateNumber(e) {
+    cardNumberEnter.textContent = e.srcElement.value;
+}
+
+function updateCvc(e) {
+    cardCvcEnter.textContent = e.srcElement.value;
+}
+
+function updateMonth(e) {
+    cardMonthEnter.textContent = e.srcElement.value;
+}
+
+function updateYear(e) {
+    cardYearEnter.textContent = e.srcElement.value;
+}
+
 document.getElementById("form-button").addEventListener("click", verificar);
 document.getElementById("send-button").addEventListener("click", restart);
 
@@ -23,7 +78,7 @@ let correctDate = false;
 let correctCvc = false;
 
 function checkName(){
-
+    
     let emptyName = 'Name cannot be empty';
     let notName = 'Please enter just letters and spaces'
 
@@ -34,9 +89,15 @@ function checkName(){
         document.getElementById('input-name').style.border = "1px solid var(--red)";
     }
     else{
-        document.getElementById('name-error').innerHTML = '';
-        document.getElementById('input-name').style.border = "1px solid var(--light-grayish-violet)";
-        correctName = true;
+        if(!onlyLetters(name)){
+            document.getElementById('name-error').innerHTML = notName;
+            document.getElementById('input-name').style.border = "1px solid var(--red)";
+        }
+        else{
+            document.getElementById('name-error').innerHTML = '';
+            document.getElementById('input-name').style.border = "1px solid var(--light-grayish-violet)";
+            correctName = true;
+        }
     }
 }
 
@@ -75,40 +136,52 @@ function checkDate(){
     let emptyDate = 'Card date (month or year) cannot be empty';
     let negativeDate = 'Card date (month or year) cannot be negative';
     let lengthDate = 'Month and year must have 2 digits';
-    let monthYearDate = 'Month between 01 and 12.'
+    let monthDate = 'Month between 01 and 12.';
+    let yearDate = 'Year is before the current year';
 
     let long = 2;
 
     let dateMonth = document.getElementById('input-month').value;
     let dateYear = document.getElementById('input-year').value;
     
+    /*Check that month and year no empty */
     if(isEmpty(dateMonth) || isEmpty(dateYear)){
         document.getElementById('date-error').innerHTML = emptyDate;
         document.getElementById('input-month').style.border = "1px solid var(--red)";
         document.getElementById('input-year').style.border = "1px solid var(--red)";
     }
     else{
+        /*Check negative month and year */
         if(isNegative(dateMonth) || isNegative(dateYear)){
             document.getElementById('date-error').innerHTML = negativeDate;
             document.getElementById('input-month').style.border = "1px solid var(--red)";
             document.getElementById('input-year').style.border = "1px solid var(--red)";
         }
         else{
+            //Check month between 01 and 12
             if(!isMonth(dateMonth)){
-                document.getElementById('date-error').innerHTML = monthYearDate;
+                document.getElementById('date-error').innerHTML = monthDate;
                 document.getElementById('input-month').style.border = "1px solid var(--red)";
             }
             else{
-                if(!correctLength(dateMonth,long) || !correctLength(dateYear,long)){
-                    document.getElementById('date-error').innerHTML = lengthDate;
+                //Check year is correct
+                if(!correctYear(dateYear)){
+                    document.getElementById('date-error').innerHTML = yearDate;
                     document.getElementById('input-month').style.border = "1px solid var(--red)";
-                    document.getElementById('input-year').style.border = "1px solid var(--red)";
                 }
                 else{
-                    document.getElementById('date-error').innerHTML = '';
-                    document.getElementById('input-month').style.border = "1px solid var(--light-grayish-violet)";
-                    document.getElementById('input-year').style.border = "1px solid var(--light-grayish-violet)";
-                    correctDate = true;
+                    //check lenght of month and year (2 digits only)
+                    if(!correctLength(dateMonth,long) || !correctLength(dateYear,long)){
+                        document.getElementById('date-error').innerHTML = lengthDate;
+                        document.getElementById('input-month').style.border = "1px solid var(--red)";
+                        document.getElementById('input-year').style.border = "1px solid var(--red)";
+                    }
+                    else{
+                        document.getElementById('date-error').innerHTML = '';
+                        document.getElementById('input-month').style.border = "1px solid var(--light-grayish-violet)";
+                        document.getElementById('input-year').style.border = "1px solid var(--light-grayish-violet)";
+                        correctDate = true;
+                    }
                 }
             }
         }
